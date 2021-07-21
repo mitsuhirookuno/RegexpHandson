af073af3b5901ca61ef52c2b5c514bf5a56bc6ae
mitsuhirookuno <mitsuhiro.okuno@gmail.com>
2021-07-21 10:41:28 +0900

    create example

---
+++ 01_select_group.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'test/unit'
+
+# FIXME
+REGEXP = /fixme/.freeze
+
+EXAMPLE = <<~EXAMPLE_INPUT.chomp
+  fukurou-labo.co.jp
+  x-dashboard.cir.io
+  fukurou-labo_co9jp
+  x-dashboard cir io
+EXAMPLE_INPUT
+
+EXPECTED = ['fukurou-labo.co.jp', 'x-dashboard.cir.io'].freeze
+
+class TestExample < Test::Unit::TestCase
+  def test_scan
+    assert_match REGEXP, EXAMPLE
+    assert_equal EXPECTED, EXAMPLE.scan(REGEXP).map(&:first)
+    assert(REGEXP.to_s.size <= 54)
+  end
+end
